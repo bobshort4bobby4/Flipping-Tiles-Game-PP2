de0547a446d4c59cedb38f47dbdc689cdfbb3f13
bobshort4bobby4 <treborken@gmail.com>
2022-02-02 16:38:39 +0000

    ADD instruction page and code to slide it

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,13 @@
 let cards = document.querySelectorAll('.card');
 cards.forEach(card => card.addEventListener('click', turnCard)); // call turnCard function when card clicked
 
+/*controls to hide/show instruction page*/
+let hideOption = window.document.getElementById("inst-btn"); // hide instruction
+hideOption.addEventListener("click", slideInstruction);
+
+let menu_btn = document.getElementById('instructions'); // show Instructions
+menu_btn.addEventListener("click", slideInstruction);
+
 
 /**
  * a function to turn cards in pairs
@@ -16,3 +23,12 @@ cards.forEach(card => card.addEventListener('click', turnCard)); // call turnCar
     this.classList.add("turn"); // turn card
 
  }
+
+ /***
+ * a function to handle the instruction page
+ */
+  function slideInstruction(){
+    let page = window.document.getElementById("instruction-page");
+     page.classList.toggle("show-instructions");
+    
+}
